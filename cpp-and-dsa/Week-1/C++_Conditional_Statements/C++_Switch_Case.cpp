6e6fa2b762c51386bf7364a246876707dbf5be62
Amuktha Goteti <amukthagoteti55@gmail.com>
2025-12-20 19:57:48 +0530

    Print Day Name and Simple Calculator using Switch Case

--- cpp-and-dsa/Week-1/C++_Conditional_Statements/C++_Switch_Case.cpp
+++ cpp-and-dsa/Week-1/C++_Conditional_Statements/C++_Switch_Case.cpp
@@ -18,5 +18,72 @@ int main() {
             cout << "Not A or B" << endl;
             break;
     }
+
+    // When using the switch statement in C++, there are few rules to keep in mind:
+    // The case values must be of int or char type.
+    // You can have as many case blocks as needed.
+    // Duplicate case values are not allowed.
+    /* 
+        A break statement is optional in each case. 
+        However it is recommended to use it 
+        otherwise the following non-matching cases would be executed until a break is found.
+    */
+
+    // Print Day Name
+    int day;
+    cout << "Enter Day Number (1-7): ";
+    cin >> day;
+    switch(day) {
+        case 1:
+            cout << "Monday" << endl;
+            break;
+        case 2:
+            cout << "Tuesday" << endl;
+            break;
+        case 3:
+            cout << "Wednesday" << endl;
+            break;
+        case 4:
+            cout << "Thursday" << endl;
+            break;
+        case 5:
+            cout << "Friday" << endl;
+            break;
+        case 6:
+            cout << "Saturday" << endl;
+            break;
+        case 7:
+            cout << "Sunday" << endl;
+            break;
+        default:
+            cout << "Invalid Day Number" << endl;
+            break;
+    }
+
+    // Simple Calculator using Switch Case
+    char operation;
+    int num1, num2;
+    cout << "Enter an operator (+, -, *, /): ";
+    cin >> operation;
+    cout << "Enter two operands: ";
+    cin >> num1 >> num2;
+
+    switch(operation) {
+        case '+':
+            cout << num1 << '+' << num2 << '=' << num1 + num2 << endl;
+            break;
+        case '-':
+            cout << num1 << '-' << num2 << '=' << num1 - num2 << endl;
+            break;
+        case '*':
+            cout << num1 << '*' << num2 << '=' << num1 * num2 << endl;
+            break;
+        case '/':
+            cout << num1 << '/' << num2 << '=' << num1 / num2 << endl;
+            break;
+        default:
+            cout << "Error! operator is not correct" << endl;
+            break;
+    }
     return 0;
 }
