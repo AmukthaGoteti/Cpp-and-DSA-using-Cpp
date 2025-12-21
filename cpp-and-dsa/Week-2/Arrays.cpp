1efce618200c2a3e6e3dfe14c3131f157b69325b
Amuktha Goteti <amukthagoteti55@gmail.com>
2025-12-21 12:08:47 +0530

    commit

--- cpp-and-dsa/Week-2/Arrays.cpp
+++ cpp-and-dsa/Week-2/Arrays.cpp
@@ -12,5 +12,41 @@ int main() {
         cout << arr[i] << " ";
     }
     cout << endl;
+
+    /*
+        We can create/declare an array by simply specifying 
+        the data type first and then the name of the array with 
+        its size inside [] square brackets
+        (better known as array subscript operator).
+        The syntax to declare an array is:
+        data_type array_name [size]
+        This statement will create an array with name array_name 
+        that can store size elements of given data_type. 
+        Once the array is declared, its size cannot be changed.
+    */
+
+    int arr1[5];
+    // When we declared an array, 
+    // the elements of array do not contain any valid value.
+
+    // Initializing means assigning initial values to aaray elements.
+    // We can initialize the array 
+    // with values enclosed in curly braces '{}' as assigned to the array.
+    int arr2[5] = {2, 4, 8, 12, 16};
+
+    // These values will be assigned sequentially.
+    // It means that 1st element (index 0) will be assigned 2,
+    // 2nd element (index 1) will be assigned 4 and so on.
+    // The number of values in the list cannot be more than 
+    // the size of the array.
+    int arr3[5] = {2, 4, 8}; // rest elements will be initialized to 0
+
+    // The size of the array can be skipped if the size should be same
+    // as the number of the values.
+    int arr4[] = {2, 4, 8, 12, 16}; // size is 5
+
+    // Moreover, all the elements can be easily initialized to 0
+    int arr5[5] = {0}; // all elements will be initialized to 0
+    // This method only works for 0, but not for any other value.
     return 0;
 }
