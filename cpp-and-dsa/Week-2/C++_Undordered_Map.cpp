d2d2b5640a5d41c153701d656a3d9d5add6d568e
Amuktha Goteti <amukthagoteti55@gmail.com>
2025-12-24 16:57:46 +0530

    commit

---
+++ cpp-and-dsa/Week-2/C++_Undordered_Map.cpp
@@ -0,0 +1,20 @@
+// In C++, `unordered_map` stores unique key-value pairs using hashing, so elements are not sorted, but searching, inserting, and deleting are fast with average O(1) time.
+/*
+    The main difference is that unordered_set stores only unique values, while unordered_map stores key–value pairs.
+
+    unordered_set: used when you only need to store and search unique elements.
+
+    unordered_map: used when each value must be linked to a unique key.
+*/
+
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int main() {
+    unordered_map<int, string> um = {{1, "Geeks"}, {2, "For"}, {3, "C++"}};
+    for(auto i : um) {
+        cout << i.first <<": " << i.second << endl;
+    }
+    return 0;
+}
