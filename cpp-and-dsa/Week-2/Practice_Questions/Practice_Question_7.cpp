7edac0df4c69054347d2056e6ef8d273517f0733
Amuktha Goteti <amukthagoteti55@gmail.com>
2026-01-02 15:25:36 +0530

    commit

---
+++ cpp-and-dsa/Week-2/Practice_Questions/Practice_Question_7.cpp
@@ -0,0 +1,62 @@
+/*
+    Problem:
+        You’re given a stack consisting of 'N' integers. Your task is to sort this stack in descending order using recursion.
+        We can only use the following functions on this stack S.
+            is_empty(S) : Tests whether stack is empty or not.
+            push(S) : Adds a new element to the stack.
+            pop(S) : Removes top element from the stack.
+            top(S) : Returns value of the top element. Note that this function does not remove elements from the stack.
+*/
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+void insertSorted(stack<int> &st, int element) {
+    if (st.empty() || st.top() <= element) {
+        st.push(element);
+        return;
+    }
+
+    int topElement = st.top();
+    st.pop();
+
+    insertSorted(st, element);
+
+    st.push(topElement);
+};
+
+void sortStack(stack<int> &st) {
+    if (st.empty()) {
+        return;
+    }
+
+    int topElement = st.top();
+    st.pop();
+
+    sortStack(st);
+
+    insertSorted(st, topElement);
+};
+
+int main() {
+    stack<int> st;
+
+    // Sample input
+    st.push(3);
+    st.push(1);
+    st.push(4);
+    st.push(2);
+    st.push(5);
+
+    // Sort the stack
+    sortStack(st);
+
+    // Print sorted stack (top to bottom)
+    while (!st.empty()) {
+        cout << st.top() << " ";
+        st.pop();
+    }
+
+    return 0;
+}
