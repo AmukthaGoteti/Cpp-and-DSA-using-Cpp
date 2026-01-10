9bef426eb46886956fb646ea6dfca304f750f847
Amuktha Goteti <amukthagoteti55@gmail.com>
2026-01-10 11:47:26 +0530

    commit

--- cpp-and-dsa/Week-3/Easy/Practice_Question_10.cpp
+++ cpp-and-dsa/Week-3/Easy/Practice_Question_10.cpp
@@ -9,6 +9,37 @@
         The large integer does not contain any leading 0's.
         Increment the large integer 
         by one and return the resulting array of digits.
+    
+    Examples:
+        Input: digits = [1,2,3]
+        Output: [1,2,4]
+        Explanation: 
+            The array represents 
+            the integer 123.
+            Incrementing by 
+            one gives 123 + 1 = 124.
+            Thus, the result 
+            should be [1,2,4].
+        
+        Input: digits = [4,3,2,1]
+        Output: [4,3,2,2]
+        Explanation: 
+            The array represents 
+            the integer 4321.
+            Incrementing 
+            by one gives 4321 + 1 = 4322.
+            Thus, the result 
+            should be [4,3,2,2].
+        
+        Input: digits = [9]
+        Output: [1,0]
+        Explanation: 
+            The array represents 
+            the integer 9.
+            Incrementing 
+            by one gives 9 + 1 = 10.
+            Thus, the result 
+            should be [1,0].
 */
 
 #include <iostream>
