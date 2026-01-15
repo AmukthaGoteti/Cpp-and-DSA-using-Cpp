77eb38992e503523f7c72abc1119d517260c3273
Amuktha Goteti <amukthagoteti55@gmail.com>
2026-01-15 13:33:28 +0530

    commit

---
+++ cpp-and-dsa/Week-4/Two_pointers_variation_and_Sliding_Window/Leetcode_Discussion_Resources/Summary_of_Sliding_Window_Patterns_for_Subarray_or_Substring.cpp
@@ -0,0 +1,13 @@
+// Summary of Sliding Window Patterns for Subarray/Substring
+/*
+    The sliding window technique is an 
+    algorithmic optimization method used for 
+    solving problems that involve contiguous 
+    subarrays or substrings.
+    This technique invloves maintaing a "Window" 
+    (a range of elements) that slides across the data structure, 
+    typically an array or a string. Instead of 
+    recalculating a value for a value for every possible subarray
+    from stractch, the sliding window reuses past computations by
+    increamentally updating the results as the window moves.
+*/
