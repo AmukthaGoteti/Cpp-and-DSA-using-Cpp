1d9047e23b40672f903aa881c3bfa78f2d08608a
Amuktha Goteti <amukthagoteti55@gmail.com>
2026-01-23 13:22:46 +0530

    commit

---
+++ cpp-and-dsa/Week-4/Sliding_Window/Hard/Practice_Question_1.cpp
@@ -0,0 +1,17 @@
+/*
+    Problem Statement:
+        Given an integer array nums and an integer k, 
+        return the length of the shortest non-empty subarray of nums with a sum of at least k. 
+        If there is no such subarray, return -1.
+        A subarray is a contiguous part of an array.
+    
+    Examples:
+        Input: nums = [1], k = 1
+        Output: 1
+
+        Input: nums = [1,2], k = 4
+        Output: -1
+
+        Input: nums = [2,-1,2], k = 3
+        Output: 3
+*/
